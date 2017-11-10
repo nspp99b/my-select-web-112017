5024476c042970fcb376b893b8440a0b2ac625c9
nspp99b <chromepanthers@gmail.com>
2017-11-09 21:53:30 -0500

    solved using control flow

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    selected_array = []
+    i = 0
+    while i < collection.length
+      selected_array.push(collection[i]) if yield collection[i]
+      i += 1
+    end
+  else
+    collection
+  end
+  selected_array
 end
